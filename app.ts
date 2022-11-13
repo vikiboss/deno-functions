aec6438b62365b55325f5831f44ce0e291411630
Viki <hi@viki.moe>
2022-11-13 15:18:11 +0800

    fix: fix favicon request

--- app.ts
+++ app.ts
@@ -1,4 +1,14 @@
-import { serve } from './deps.ts';
-import { fetchReadmeToHtml } from './utils/fetchReadmeToHtml.ts';
+import { serve } from './deps.ts'
+import { fetchReadmeToHtml } from './utils/fetchReadmeToHtml.ts'
 
-serve(() => fetchReadmeToHtml(import.meta.url));
+const handleRequest = async (request: Request) => {
+  const url = new URL(request.url)
+
+  if (url.pathname === '/favicon.ico') {
+    return new Response(null)
+  }
+
+  return await fetchReadmeToHtml(import.meta.url)
+}
+
+serve(handleRequest)
