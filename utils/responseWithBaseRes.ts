dad70a248d7eb4e72682db1938543e5009ee7542
Viki <hi@viki.moe>
2022-11-13 15:01:49 +0800

    feat: add base res

---
+++ utils/responseWithBaseRes.ts
@@ -0,0 +1,19 @@
+export const responseWithBaseRes = (
+  obj: Record<number | string | symbol, any> | undefined,
+  status = 200,
+  message = 'OK'
+) => {
+  let res = ''
+
+  try {
+    res = JSON.stringify({ status, message, data: obj || {} })
+  } catch {
+    res = JSON.stringify({ status, message: 'Oops', data: {} })
+  }
+
+  return new Response(res, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
